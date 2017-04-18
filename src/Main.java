d65790473d5ae14dae741e6a7a179e0a9d8dcc67
Cristian <you@example.com>
2017-04-18 16:13:15 -0300

    tarea encontrar impares, multiplos de 7  y los pares excepto el 24 y el 72

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i <= 100; i++) {
+            if (i % 2 == 0) System.out.println("Los numero pares son : " + i);
+
+        }
+
+        // for(i=0;i<=100;i++){
+        //    if(i % 7 == 0)
+        //        System.out.println(" Los multiplos de 7 son :" + i);
+        //  }
+
+
+        // for (i=1; i<= 100; i++){
+        //     if (i%2!=0) System.out.println("Los numero inpares son : " + i);
+        // };
+    }
+}
+
+
